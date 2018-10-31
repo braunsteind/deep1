e24328a2afedb1eb0fd965d75a349545f3c4276c
Coral Malachi <38204874+CoralMalachi@users.noreply.github.com>
2018-10-31 08:37:53 +0200

    Update train_loglin.py

--- train_loglin.py
+++ train_loglin.py
@@ -1,20 +1,47 @@
-import loglinear as ll
-import random
 import numpy as np
-import utils
+import loglinear as ll
+import utils as utils
+import random
 
-STUDENT = {'name': 'YOUR NAME',
-           'ID': 'YOUR ID NUMBER'}
+STUDENT={'name': 'YOUR NAME',
+         'ID': 'YOUR ID NUMBER'}
 
+def vector_normalization(vec):
+    vector_sum = np.sum(vec)
+    return np.divide(vec,vector_sum)
 
 def feats_to_vec(features):
     # YOUR CODE HERE.
     # Should return a numpy vector of features.
-    vec = np.zeros(600)
-    for feats in features:
-        if feats in utils.F2I:
-            vec[utils.F2I[feats]] += 1
-    return vec
+    #find the number of id - how many different bigrams
+    num_of_ids = len(utils.F2I)
+    vec = np.zeros(num_of_ids)
+    for bigram in features:
+        if bigram in utils.F2I:
+            m_id = utils.F2I[bigram]
+            #update count
+            vec[m_id] = 1 + vec[m_id]
+
+    #we need to do normalization to the input layer in order to prevent
+    #situation of to big numbers
+
+    return vector_normalization(vec)
+
+def CreatePredictionsFile(data, parameters):
+    file_predictions = open("test.pred", 'w')
+    #list of languages
+    languages_list = utils.L2I.items()
+    for tag, features in data:
+        x = feats_to_vec(features)  # convert features to a vector.
+        predicted_language = ll.predict(x, parameters)
+        for language, text in languages_list:  # for name, age in dictionary.iteritems():  (for Python 2.x)
+            if predicted_language == text:
+                tag = language
+                break
+        file_predictions.write(str(tag) + "\n")
+        #close the file
+    file_predictions.close()
+
 
 
 def accuracy_on_dataset(dataset, params):
@@ -24,15 +51,14 @@ def accuracy_on_dataset(dataset, params):
         # Compute the accuracy (a scalar) of the current parameters
         # on the dataset.
         # accuracy is (correct_predictions / all_predictions)
-        x = feats_to_vec(features)
-        y = utils.L2I[label]
-        y_hat = ll.predict(x, params)
-        if y == y_hat:
-            good += 1
+        vec_features = feats_to_vec(features)
+        y_predict = ll.predict(vec_features,params)
+        language_label = utils.L2I[label]
+        if y_predict == language_label:
+            good = good+1
         else:
-            bad += 1
+            bad = bad+1
     return good / (good + bad)
-
 
 def train_classifier(train_data, dev_data, num_iterations, learning_rate, params):
     """
@@ -45,18 +71,24 @@ def train_classifier(train_data, dev_data, num_iterations, learning_rate, params
     params: list of parameters (initial values)
     """
     for I in xrange(num_iterations):
-        cum_loss = 0.0  # total loss in this iteration.
+        cum_loss = 0.0 # total loss in this iteration.
         random.shuffle(train_data)
         for label, features in train_data:
-            x = feats_to_vec(features)  # convert features to a vector.
-            y = utils.L2I[label]  # convert the label to number if needed.
-            loss, grads = ll.loss_and_gradients(x, y, params)
+            # convert features to a vector.
+            x = feats_to_vec(features)
+            language_label = utils.L2I[label]
+            y = language_label                  # convert the label to number if needed.
+            loss, grads = ll.loss_and_gradients(x,y,params)
             cum_loss += loss
             # YOUR CODE HERE
             # update the parameters according to the gradients
             # and the learning rate.
-            params[0] -= learning_rate * grads[0]
-            params[1] -= learning_rate * grads[1]
+            #update b matrix - rule update : b = b -n * gradientB
+            params[1] = params[1] -learning_rate*grads[1]
+
+            # update W matrix - rule update : w = w -n * gradientW
+            params[0] = params[0] - learning_rate * grads[0]
+
         train_loss = cum_loss / len(train_data)
         train_accuracy = accuracy_on_dataset(train_data, params)
         dev_accuracy = accuracy_on_dataset(dev_data, params)
@@ -68,14 +100,17 @@ if __name__ == '__main__':
     # YOUR CODE HERE
     # write code to load the train and dev sets, set up whatever you need,
     # and call train_classifier.
-
+    
     # ...
-    in_dim = 600
-    out_dim = 6
+    #number of features - different bigrams - input layer dim
+    in_dim = len(utils.F2I)
+    #number of languages - output layer dim
+    out_dim = len(utils.L2I)
     train_data = utils.TRAIN
     dev_data = utils.DEV
-    num_iterations = 3
-    learning_rate = 0.3
+    num_iterations = 50
+    learning_rate = 0.05
 
     params = ll.create_classifier(in_dim, out_dim)
     trained_params = train_classifier(train_data, dev_data, num_iterations, learning_rate, params)
+    CreatePredictionsFile(train_data,trained_params)

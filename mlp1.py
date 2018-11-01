4c74804f0a1c2c6de0dd3587d6fa9f356b734f8d
Coral Malachi <38204874+CoralMalachi@users.noreply.github.com>
2018-11-01 14:58:14 +0200

    Add files via upload

--- mlp1.py
+++ mlp1.py
@@ -1,10 +1,14 @@
+import loglinear as ll
 import numpy as np
+
 
 STUDENT={'name': 'YOUR NAME',
          'ID': 'YOUR ID NUMBER'}
 
 def classifier_output(x, params):
     # YOUR CODE HERE.
+    feed_forward_model = forward_propagation_action(x,params)
+    probs =  feed_forward_model['A2']
     return probs
 
 def predict(x, params):
@@ -38,6 +42,40 @@ def create_classifier(in_dim, hid_dim, out_dim):
     return:
     a flat list of 4 elements, W, b, U, b_tag.
     """
-    params = []
+    params = [W,b,U,b_tag]
     return params
+
+def than(z):
+    return np.tanh(z)
+
+def forward_propagation_action(x,params):
+    # first load the model parameters
+    w1 = params[0]
+    b1 = params[1]
+    w2 = params[2]
+    b2 = params[3]
+
+    # compute Z1: input layer matrix dot w1 wheight matrix plus our bias
+    z1 = np.dot(w1,x)+b1
+
+    # put it throgh our activition function
+    A1 = than(z1)
+
+    # compute Z2:
+    z2 =np.dot(w2,A1)+b2
+
+    # now, we'll use the softmax as our activition function
+    A2 = ll.softmax(z2)
+
+    # save all results as a model
+    result_model = {'A0': x,
+                    'z1': z1,
+                    'A1': A1,
+                    'z2': z2,
+                    'A2': A2}
+
+
+    return result_model
+
+
 
